2aa45d0948cd2fceccd2c7f79999a9b6c04567a5
dem4ply <dem4ply@gmail.com>
2019-01-30 22:50:25 -0600

    se movieron los systemas

---
+++ Assets/_script/chibi/system/weapon/gun/Gun.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using Unity.Entities;
+using chibi.motor;
+
+namespace chibi.systems.weapon.gun
+{
+	public class Gun : ComponentSystem
+	{
+		struct group
+		{
+			public chibi.weapon.gun.Gun gun;
+		}
+
+		protected override void OnUpdate()
+		{
+			foreach ( var entity in GetEntities<group>() )
+			{
+				Debug.Log( entity.gun.gameObject.name );
+			}
+		}
+	}
+}

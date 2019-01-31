dd3bc088dd48fca05fe9d84b19bc15777c41bc20
dem4ply <dem4ply@gmail.com>
2019-01-30 22:21:07 -0600

    se agregan weapon

---
+++ Assets/_script/chibi/weapon/Weapon.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace chibi.weapon
+{
+	public abstract class Weapon : Chibi_behaviour
+	{
+		public rol_sheet.Rol_sheet owner;
+
+		public abstract void attack();
+	}
+}

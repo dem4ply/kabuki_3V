9b106a62f4754348bced7ccd9d8799a4a780db8c
dem4ply <dem4ply@gmail.com>
2019-02-03 15:30:35 -0600

    preparativos apra el controller de soldados

---
+++ Assets/_script/chibi/Controller/npc/Soldier_controller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace chibi.controller.npc
+{
+	public class Soldier_controller : Controller
+	{
+		public chibi.controller.weapon.gun.turrent.Controller_turrent turrent;
+		public chibi.controller.npc.Controller_npc npc;
+		public Transform hold_turrent_position;
+
+		public void change_owner_turrent()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void release_turrent()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void hold_turrent()
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
---
+++ Assets/_script/chibi/Controller/trigger/Turrent_hold_area.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace chibi.controller.trigger
+{
+	public class Turrent_hold_area : chibi_base.Chibi_behaviour
+	{
+		public weapon.gun.turrent.Controller_turrent turrent;
+		public Transform hold_position;
+
+		protected virtual void OnTriggerEnter( Collider other )
+		{
+			Debug.Log(
+				string.Format(
+					"[Turrent_hold_area] entro en el area {0}",
+					helper.game_object.name.full( other.gameObject ) ) );
+			var soldier = other.GetComponent<npc.Soldier_controller>();
+			if ( soldier )
+			{
+				soldier.turrent = turrent;
+				soldier.hold_turrent_position = hold_position;
+			}
+		}
+	}
+}

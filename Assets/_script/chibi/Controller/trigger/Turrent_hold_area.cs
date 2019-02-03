2ba08d4404b6aa19ff0210d7bc3237e132fb089e
dem4ply <dem4ply@gmail.com>
2019-02-03 16:01:23 -0600

    pruebas para turrent hold area

--- Assets/_script/chibi/Controller/trigger/Turrent_hold_area.cs
+++ Assets/_script/chibi/Controller/trigger/Turrent_hold_area.cs
@@ -22,5 +22,22 @@ namespace chibi.controller.trigger
 				soldier.hold_turrent_position = hold_position;
 			}
 		}
+
+		protected virtual void OnTriggerExit( Collider other )
+		{
+			Debug.Log(
+				string.Format(
+					"[Turrent_hold_area] salio del area {0}",
+					helper.game_object.name.full( other.gameObject ) ) );
+			var soldier = other.GetComponent<npc.Soldier_controller>();
+			if ( soldier )
+			{
+				if ( soldier.turrent == turrent )
+				{
+					soldier.turrent = null;
+					soldier.hold_turrent_position = null;
+				}
+			}
+		}
 	}
 }
